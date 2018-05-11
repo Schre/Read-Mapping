1af2ff6f2b6d1d8b351ae9bee5de36c71e739d7e
Sam <samuel.schreiber@wsu.edu>
2018-05-11 08:10:37 -0700

    Final version

--- Utilities.h
+++ Utilities.h
@@ -12,9 +12,8 @@ long int max2(long int arg1, long int arg2){
 long int max3(long int arg1, long int arg2, long int arg3){
   return max2(max2(arg1,arg2),max2(arg2,arg3));
 }
-int OutputAlignment(string & s1, string & aligned, int outputLength, int offset1, int offset2 ,int m, int mi, int h, int g, int isLocal){
-  int ret = 0;
-  int start = 0;
+std::pair<int,int> OutputAlignment(string & s1, string & aligned, int outputLength, int & start, int m, int mi, int h, int g, int isLocal, bool print=false){
+  std::pair<int,int> ret;
   int s1gaps = 0, alignedgaps = 0;
   int s1indx = 0, alignedindx = 0;
   int matches = 0, mismatches = 0, gaps = 0, opening_gaps = 0;
@@ -36,18 +35,21 @@ int OutputAlignment(string & s1, string & aligned, int outputLength, int offset1
       else
 	++s1indx;
     }
-    
-    printf("s1\t %d\t %s", t1+1 + offset1, s1.substr(start, outputLength).c_str());
-    printf("\t %lu\n", (unsigned long)(start + offset1 + outputLength-s1gaps));
-    printf("  \t  \t ");
+    if (print){
+      printf("s1\t %d\t %s", t1+1, s1.substr(start, outputLength).c_str());
+      printf("\t %lu\n", (unsigned long)(start + outputLength-s1gaps));
+      printf("  \t  \t ");
+    }
     for (int i = start; i < outputLength+start; ++i){
       if (s1[i] == aligned[i]){
-	++ret;
-	printf("|");
+	++ret.first;
+	if (print)
+	  printf("|");
 	++matches;
       }
       else{
-	printf(" ");
+	if (print)
+	  printf(" ");
 	++mismatches;
       }
       if(aligned[i] == '-'){
@@ -61,14 +63,18 @@ int OutputAlignment(string & s1, string & aligned, int outputLength, int offset1
       else
 	++alignedindx;
     }
-    printf("\n");
-    printf("s2\t %d\t %s\t %lu\n\n", t2+1+offset2,aligned.substr(start,outputLength).c_str(),(unsigned long)(start + offset2 + outputLength - alignedgaps));
+    if (print){
+      printf("\n");
+      printf("s2\t %d\t %s\t %lu\n\n", t2+1,aligned.substr(start,outputLength).c_str(),(unsigned long)(start + outputLength - alignedgaps));
+    }
     start+= outputLength;
   }
   gaps = s1gaps + alignedgaps;
   mismatches-= gaps;
   int totalLen = mismatches + matches + gaps;
-  std::cout << std::endl;
+  if (print)
+    std::cout << std::endl;
+  if (print){
   std::cout << "Report\n\n";
   if (!isLocal)
     std::cout << "Global ";
@@ -77,5 +83,9 @@ int OutputAlignment(string & s1, string & aligned, int outputLength, int offset1
   std::cout << "optimal score = " << (matches*m)+(mismatches*mi)+(gaps*g)+(opening_gaps*h) << "\n\n";
   std::cout << "Number of: matches = " << matches << ",mismatches = " << mismatches << ",gaps = " << gaps << ",opening gaps = " << opening_gaps << std::endl;
   printf("Identities = %d/%d (%d%%), Gaps = %d/%d = (%d%%)\n",matches,totalLen,(int)(round(100*(double)matches/(totalLen))), gaps, totalLen, (int)(round(100*(double)gaps/(totalLen))));
+  }
+  ret.second = matches + mismatches + gaps;
+  if (print)
+    std::cout << "Matched: " << ret.first << " Aligned Length: " << ret.second << '\n';
   return ret;
 }
